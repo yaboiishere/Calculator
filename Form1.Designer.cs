71ccdbb4b1b146fe5fdc49feef98ce0623a2bb02
Mihail Dobrev <mixaildobrev@gmail.com>
2020-03-09 22:18:37 +0200

    added "-", "*" and "/" also clearing error message on success

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -77,6 +77,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -87,6 +88,7 @@
             this.button3.TabIndex = 4;
             this.button3.Text = "*";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -97,6 +99,7 @@
             this.button4.TabIndex = 5;
             this.button4.Text = "/";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
             // 
--- Form1.cs
+++ Form1.cs
@@ -8,25 +8,56 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Calculator
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace Calculator{
+    public partial class Form1 : Form{
+        public Form1(){
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
+        private void button1_Click(object sender, EventArgs e){
+            try{
                 double a = double.Parse(textBox1.Text);
                 double b = double.Parse(textBox2.Text);
                 label2.Text = (a + b).ToString();
+                label4.Text = "";
             }
-            catch (FormatException)
-            {
+            catch (FormatException){
+                label4.Text = "Моля попълни полетата";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e){
+            try{
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                label2.Text = (a - b).ToString();
+                label4.Text = "";
+            }
+            catch (FormatException){
+                label4.Text = "Моля попълни полетата";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e){
+            try{
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                label2.Text = (a * b).ToString();
+                label4.Text = "";
+            }
+            catch (FormatException){
+                label4.Text = "Моля попълни полетата";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e){
+            try{
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                label2.Text = (a / b).ToString();
+                label4.Text = "";
+            }
+            catch (FormatException){
                 label4.Text = "Моля попълни полетата";
             }
         }
